8f86ca3cda07b23c17e471ee5c9fb9b24503fb53
commandiron <commandiron@gmail.com>
2023-01-13 18:04:14 +0300

    core.

--- lib/provider/scroll_provider.dart
+++ lib/provider/scroll_provider.dart
@@ -9,29 +9,14 @@ class ScrollProvider extends ChangeNotifier {
 
   scrollDesktop(int index) {
     double offset = index == 1
-        ? 1000 - AppSize.navBarSize!
-        : index == 2
-            ? 1800 - AppSize.navBarSize!
-            : index == 3
-                ? 2600 - AppSize.navBarSize!
-                : 0;
+      ? 1000 - AppSize.navBarSize!
+      : index == 2
+        ? 1800 - AppSize.navBarSize!
+        : index == 3
+          ? 2600 - AppSize.navBarSize!
+          : 0;
     controller.animateTo(
       offset,
-      duration: const Duration(seconds: 1),
-      curve: Curves.easeInOut,
-    );
-  }
-
-  scrollTabletMobile(int index) {
-    double offset = index == 1
-        ? 290
-        : index == 2
-            ? 360
-            : index == 3
-                ? 300
-                : 310;
-    controller.animateTo(
-      offset * index,
       duration: const Duration(seconds: 1),
       curve: Curves.easeInOut,
     );
--- lib/sections/products/products.dart
+++ lib/sections/products/products.dart
@@ -1,7 +1,3 @@
-import 'package:demirli_tech_website/configs/app_padding.dart';
-import 'package:demirli_tech_website/configs/app_size.dart';
-import 'package:demirli_tech_website/configs/app_space.dart';
-import 'package:demirli_tech_website/configs/app_text.dart';
 import 'package:flutter/material.dart';
 
 class Products extends StatelessWidget {
@@ -14,46 +10,6 @@ class Products extends StatelessWidget {
       height: 800,
       color: Theme.of(context).colorScheme.primaryContainer,
       alignment: Alignment.topCenter,
-      child: Column(
-        children: [
-          Container(
-            height: AppSize.navBarSize,
-            alignment: Alignment.center,
-            child: Text("Ürünler", style: AppText.h1?.copyWith(color: Colors.white),),
-          ),
-          Expanded(
-            child: Padding(
-              padding: EdgeInsets.only(
-                left: 8,
-                right: 8,
-                bottom: 8
-              ),
-              child: Row(
-                children: [
-                  Expanded(
-                      child: Padding(
-                        padding: AppPadding.allS!,
-                        child: Container(color: Colors.red,),
-                      )
-                  ),
-                  Expanded(
-                    child: Padding(
-                      padding: AppPadding.allS!,
-                      child: Column(
-                        children: [
-                          Expanded(child: Container(color: Colors.blue)),
-                          AppSpace.yLarge!,
-                          Expanded(child: Container(color: Colors.green)),
-                        ],
-                      ),
-                    )
-                  )
-                ],
-              ),
-            )
-          )
-        ],
-      ),
     );
   }
 }

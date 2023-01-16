9ccf4e1e83a77b80752aee6aaf5fd15677e2b563
commandiron <commandiron@gmail.com>
2023-01-16 19:23:12 +0300

    core.

--- lib/sections/products/products_desktop.dart
+++ lib/sections/products/products_desktop.dart
@@ -2,6 +2,7 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:demirli_tech_website/configs/app_padding.dart';
 import 'package:demirli_tech_website/configs/app_size.dart';
+import 'package:demirli_tech_website/configs/app_space.dart';
 import 'package:flutter/material.dart';
 import '../../configs/app_text.dart';
 
@@ -53,7 +54,6 @@ class _ProductsDesktopState extends State<ProductsDesktop> {
             child: CarouselSlider(
               carouselController: _carouselController,
               options: CarouselOptions(
-                autoPlay: true,
                 viewportFraction: 1.0,
                 height: AppSize.productsSectionHeight
               ),
@@ -63,7 +63,43 @@ class _ProductsDesktopState extends State<ProductsDesktop> {
                     return Container(
                       alignment: Alignment.topLeft,
                       padding: AppPadding.allXXL,
-                      child: Text('text $i', style: AppText.h1!.copyWith(color: Colors.white),)
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                SizedBox(
+                                  width: 96,
+                                  child: Image.asset("assets/beson_logo.png",),
+                                ),
+                                AppSpace.verticalL!,
+                                Container(
+                                  padding: AppPadding.horizontalS,
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Text('Konut Maliyet', style: AppText.h2b!.copyWith(color: Colors.white),),
+                                      AppSpace.verticalXL!,
+                                      Text("Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?", style: AppText.b2!.copyWith(color: Colors.white),),
+                                      AppSpace.verticalXL!,
+                                      Image.asset("assets/app_store_badge.png",),
+                                      AppSpace.verticalXL!,
+                                      Image.asset("assets/google_play_badge.png",),
+                                    ]
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                          Expanded(
+                            child: Container(
+                              padding: AppPadding.allL,
+                              child: Image.asset("assets/beson_spash.png",),
+                            )
+                          ),
+                        ],
+                      )
                     );
                   },
                 );

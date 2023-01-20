0fd7245ec5e425e3f5f42c6589a4af0b6c7982eb
commandiron <commandiron@gmail.com>
2023-01-20 14:59:39 +0300

    core.

--- lib/model/company_contact.dart
+++ lib/model/company_contact.dart
@@ -15,6 +15,10 @@ class CompanyContact {
       imagePath: "assets/images/instagram_icon_26.png",
     ),
     CompanyContact(
+      url: "https://facebook.com/emirdemirli",
+      imagePath: "assets/images/facebook_icon_512.png",
+    ),
+    CompanyContact(
       url: "https://api.whatsapp.com/send?phone=905355085552",
       imagePath: "assets/images/whatsapp_icon_26.png",
     ),

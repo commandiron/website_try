2647463e516c42e16267e3c0bac24f0d413c308a
commandiron <commandiron@gmail.com>
2023-01-23 16:33:14 +0300

    core.

--- lib/sections/contact/contact_desktop.dart
+++ lib/sections/contact/contact_desktop.dart
@@ -1,3 +1,4 @@
+import 'package:demirli_tech_website/configs/app_padding.dart';
 import 'package:demirli_tech_website/configs/app_space.dart';
 import 'package:demirli_tech_website/configs/app_text_style.dart';
 import 'package:demirli_tech_website/model/company_contact.dart';
@@ -18,85 +19,90 @@ class ContactDesktop extends StatelessWidget {
       child: Column(
         children: [
           const ContactTitle(),
-          SelectionArea(
-            child: Column(
-              children: [
-                Text("Sosyal Medya", style: AppTextStyle.h3!.copyWith(color: Colors.white)),
-                AppSpace.verticalM!,
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: CompanyContact.items.map(
-                    (companyContact) {
-                      return ContactIconButton(
-                        iconPath: companyContact.iconPath,
-                        iconOriginalColor: companyContact.iconOriginalColor,
-                        onTap: () => openURL(companyContact.url),
-                      );
-                    }
-                  ).toList(),
-                ),
-                AppSpace.verticalL!,
-                Text("Eposta", style: AppTextStyle.h3!.copyWith(color: Colors.white)),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    AppSpace.horizontalL!,
-                    Text("info@demirli.tech", style: AppTextStyle.b1!.copyWith(color: Colors.white)),
-                    AppSpace.horizontalM!,
-                    InkWell(
-                      onTap: () => openURL("mailto:info@demirli.tech"),
-                      child: Icon(
-                        Icons.mail,
-                        color: Theme.of(context).colorScheme.primary,
-                        size: 16,
-                      ),
-                    ),
-                  ],
-                ),
-                AppSpace.verticalL!,
-                Text("Ofis", style: AppTextStyle.h3!.copyWith(color: Colors.white)),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    AppSpace.horizontalL!,
-                    Text("Yıldızbakkal Taşköprü Cad. Demirli İş Merkezi Kadıköy - İSTANBUL", style: AppTextStyle.b1!.copyWith(color: Colors.white)),
-                    AppSpace.horizontalM!,
-                    InkWell(
-                      onTap: () => openURL("https://goo.gl/maps/RNiqv558Wzu3nqeR7"),
-                      child: Icon(
-                        Icons.location_on,
-                        color: Theme.of(context).colorScheme.primary,
-                        size: 16,
-                      ),
-                    ),
-                  ],
-                ),
-                AppSpace.verticalL!,
-                Text("Telefon", style: AppTextStyle.h3!.copyWith(color: Colors.white)),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    if(!kIsWeb)
+          Padding(
+            padding: AppPadding.horizontalL!,
+            child: SelectionArea(
+              child: Column(
+                children: [
+                  Text("Sosyal Medya", style: AppTextStyle.h3!.copyWith(color: Colors.white)),
+                  AppSpace.verticalM!,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: CompanyContact.items.map(
+                      (companyContact) {
+                        return ContactIconButton(
+                          iconPath: companyContact.iconPath,
+                          iconOriginalColor: companyContact.iconOriginalColor,
+                          onTap: () => openURL(companyContact.url),
+                        );
+                      }
+                    ).toList(),
+                  ),
+                  AppSpace.verticalL!,
+                  Text("Eposta", style: AppTextStyle.h3!.copyWith(color: Colors.white)),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
                       AppSpace.horizontalL!,
-                    Text("+90 535 508 55 52", style: AppTextStyle.b1!.copyWith(color: Colors.white)),
-                    if(!kIsWeb)
-                      Row(
-                        children: [
-                          AppSpace.horizontalM!,
-                          InkWell(
-                            onTap: () => openURL("tel:+90 535 508 55 52"),
-                            child: Icon(
-                              Icons.phone,
-                              color: Theme.of(context).colorScheme.primary,
-                              size: 16,
-                            ),
+                      Text("info@demirli.tech", style: AppTextStyle.b1!.copyWith(color: Colors.white)),
+                      AppSpace.horizontalM!,
+                      InkWell(
+                        onTap: () => openURL("mailto:info@demirli.tech"),
+                        child: Icon(
+                          Icons.mail,
+                          color: Theme.of(context).colorScheme.primary,
+                          size: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                  AppSpace.verticalL!,
+                  Text("Ofis", style: AppTextStyle.h3!.copyWith(color: Colors.white)),
+                  FittedBox(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        AppSpace.horizontalL!,
+                        Text("Yıldızbakkal Taşköprü Cad. Demirli İş Merkezi Kadıköy - İSTANBUL", style: AppTextStyle.b1!.copyWith(color: Colors.white)),
+                        AppSpace.horizontalM!,
+                        InkWell(
+                          onTap: () => openURL("https://goo.gl/maps/RNiqv558Wzu3nqeR7"),
+                          child: Icon(
+                            Icons.location_on,
+                            color: Theme.of(context).colorScheme.primary,
+                            size: 16,
                           ),
-                        ],
-                      )
-                  ],
-                ),
-              ],
-            )
+                        ),
+                      ],
+                    ),
+                  ),
+                  AppSpace.verticalL!,
+                  Text("Telefon", style: AppTextStyle.h3!.copyWith(color: Colors.white)),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      if(!kIsWeb)
+                        AppSpace.horizontalL!,
+                      Text("+90 535 508 55 52", style: AppTextStyle.b1!.copyWith(color: Colors.white)),
+                      if(!kIsWeb)
+                        Row(
+                          children: [
+                            AppSpace.horizontalM!,
+                            InkWell(
+                              onTap: () => openURL("tel:+90 535 508 55 52"),
+                              child: Icon(
+                                Icons.phone,
+                                color: Theme.of(context).colorScheme.primary,
+                                size: 16,
+                              ),
+                            ),
+                          ],
+                        )
+                    ],
+                  ),
+                ],
+              )
+            ),
           ),
           const ContactFooter()
         ],

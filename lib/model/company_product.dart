ec2f2a80a920812d82fd9ef574ca25f24caab36d
commandiron <commandiron@gmail.com>
2023-01-24 17:58:25 +0300

    core.

--- lib/model/company_product.dart
+++ lib/model/company_product.dart
@@ -38,9 +38,9 @@ class CompanyProduct {
       imageAssetPath: "assets/images/product/kost/example_cover_for_demirli_tech.png"
     ),
     CompanyProduct(
-      logoAssetPath: "assets/images/product/qr_projem/qr_projem_logo.png",
+      logoAssetPath: "assets/images/product/qr_projem/qr_projem_logo_100.png",
       name: "Qr Projem",
-      shortDescription: "Qr kod ile inşaat proje broşürü oluşturma uygulaması.",
+      shortDescription: "İnşaat projeleri için Qr kod ile bilgi broşürü oluşturma uygulaması.",
       fullDescription: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. \n \nNeque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
       imageAssetPath: "assets/images/product/kost/example_cover_for_demirli_tech.png"
     ),

887f4849ce9880de85c912b89a7276cf6363fade
commandiron <commandiron@gmail.com>
2023-01-25 18:19:33 +0300

    core.

--- lib/model/company_product.dart
+++ lib/model/company_product.dart
@@ -32,11 +32,11 @@ class CompanyProduct {
       stage: Stage.research
     ),
     CompanyProduct(
-      logoAssetPath: "assets/images/product/oto_plan/oto_plan_logo_100.png",
-      name: "Kolay Plan",
+      logoAssetPath: "assets/images/product/planok/planok_logo_100.png",
+      name: "Planok",
       shortDescription: "Küçük ve orta ölçekli inşaat projeleri için planlama ve takip uygulaması",
       fullDescription: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. \n \nNeque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
-      imageAssetPath: "assets/images/product/oto_plan/oto_plan_cover.png",
+      imageAssetPath: "assets/images/product/planok/planok_cover.png",
       stage: Stage.research
     ),
     CompanyProduct(
